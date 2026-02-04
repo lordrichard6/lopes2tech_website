2b7069a82905d2627059929fbd6febd7d49529ad
lordrichard6 <paulolopesreizinho@gmail.com>
2026-02-04 09:32:39 +0100

    fix(i18n): Use locale-aware Link in footer
    - Changed from next/link to next-intl Link in Footer component
    
    - Footer links now properly include locale prefix (/{locale}/page)
    
    - Fixes broken links to Impressum, Privacy Policy, Terms of Service

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/link";
+import { Link } from "@/navigation";
 import Image from "next/image";
 import { Mail, Linkedin, Instagram, FileText } from "lucide-react";
 import { useTranslations } from "next-intl";
